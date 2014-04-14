5497d434722052e6ccb16582fc73bc0260fd0923
Dan Fairbanks <fairbanksdan@gmail.com>
2014-04-14 11:24:30 -0700

    Initial Commit

---
+++ autoLayoutDay1Tests/autoLayoutDay1Tests.m
@@ -0,0 +1,34 @@
+//
+//  autoLayoutDay1Tests.m
+//  autoLayoutDay1Tests
+//
+//  Created by Daniel Fairbanks on 4/14/14.
+//  Copyright (c) 2014 Fairbanksdan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface autoLayoutDay1Tests : XCTestCase
+
+@end
+
+@implementation autoLayoutDay1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
